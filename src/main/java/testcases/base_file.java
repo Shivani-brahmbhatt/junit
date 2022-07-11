376fb5fe54f945badf30c097727aed759f746e46
Shivani Brahmbhatt <108492648+Shivani-brahmbhatt@users.noreply.github.com>
2022-07-11 11:17:29 -0400

    Update base_file.java

--- src/main/java/testcases/base_file.java
+++ src/main/java/testcases/base_file.java
@@ -6,11 +6,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,31 +24,56 @@ public class base_file {
 
 	static junit_readconfig read = new junit_readconfig();
 	public static String baseurl = read.urlcall();
-	public static String browser = read.returnchromepath();
-
+	public String browser = read.returnchromepath();
+	public String browser2 = read.returnfirepath();
 	public String item_base = read.item1();
-
+	private static final String DRIVER_FIREFOX = "firefox";
+	private static final String DRIVER_CHROME = "chrome";
 	public static WebDriver driver;
+	public String exptitle = "Plumbing Supplies, HVAC Parts, Pipe, Valves & Fittings – Ferguson";
+
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
 
 	@Before
 	public void setup() {
 
 		DesiredCapabilities capabilities;
-		// Which driver to use?
-		capabilities = DesiredCapabilities.chrome();
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver(capabilities);
-		driver.get("https://www.ferguson.com");
 
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+		if (DRIVER_CHROME.equalsIgnoreCase(browser)) {
 
+			capabilities = DesiredCapabilities.chrome();
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver(capabilities);
+			driver.get("https://www.ferguson.com");
+
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		} else if (DRIVER_FIREFOX.equalsIgnoreCase(browser2)) {
+			capabilities = DesiredCapabilities.firefox();
+			driver = new FirefoxDriver(capabilities);
+			driver.get("https://www.ferguson.com");
+
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		}
+
+	}
+
+	@Test
+	public void verifytitle() {
+		String actualtitle = driver.getTitle();
+		try {
+			Assert.assertEquals(exptitle, actualtitle);
+		} catch (Exception e) {
+			collector.addError(e);
+		}
 	}
 
 	@After
 	public void teardown() {
-		driver.close();
-		driver.quit();
+		// driver.close();
+		// driver.quit();
 	}
 
 	public void realshot(WebDriver driver, String screenshotname) throws IOException {
@@ -51,7 +81,8 @@ public class base_file {
 		TakesScreenshot screenshot = ((TakesScreenshot) driver);// we are using this driver from base class which must
 																// be satatic so we can use
 		File src = screenshot.getScreenshotAs(OutputType.FILE);
-		String screenshotpath1 = ("C:\\Users\\Shivani\\eclipse-workspace\\junit\\screenshot\\" + screenshotname+ ".png");
+		String screenshotpath1 = ("C:\\Users\\Shivani\\eclipse-workspace\\junit\\screenshot\\" + screenshotname
+				+ ".png");
 		// File dest=new
 		// File(System.getProperty("User.dir")+"\\screenshots\\"+screenshotname+".png");
 		File dest = new File(screenshotpath1);
